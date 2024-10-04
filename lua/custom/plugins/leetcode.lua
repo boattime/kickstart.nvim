9d230925a978ab18d2d20be4f5ea70572f58817f
Max Teibel <mteibel@amazon.com>
2024-10-04 09:57:44 -0400

    Add Leetcode plugin and install clangd LSP

---
+++ lua/custom/plugins/leetcode.lua
@@ -0,0 +1,18 @@
+return {
+  'kawre/leetcode.nvim',
+  build = ':TSUpdate html',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'nvim-lua/plenary.nvim', -- required by telescope
+    'MunifTanjim/nui.nvim',
+
+    -- optional
+    'nvim-treesitter/nvim-treesitter',
+    'rcarriga/nvim-notify',
+    'nvim-tree/nvim-web-devicons',
+  },
+  -- Template file https://github.com/kawre/leetcode.nvim/blob/master/lua/leetcode/config/template.lua
+  opts = {
+    lang = 'cpp',
+  },
+}
