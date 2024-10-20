4c091652860f70f19b6efa187ab9bab4b840635c
Max <max@depositlink.com>
2024-10-20 07:31:46 -0400

    Update obsidian configuration

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -30,10 +30,31 @@ return {
     --  * "notes_subdir" - put new notes in the default notes subdirectory.
     new_notes_location = 'notes_subdir',
 
+    -- Optional, alternatively you can customize the frontmatter data.
+    ---@return table
+    note_frontmatter_func = function(note)
+      -- Add the title of the note as an alias.
+      if note.title then
+        note:add_alias(note.title)
+      end
+
+      local out = { id = note.id, aliases = note.aliases, tags = note.tags, project = '', area = '' }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
+      return out
+    end,
+
     -- Optional, boolean or a function that takes a filename and returns a boolean.
     -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
     disable_frontmatter = function(filename)
-      local dirName = 'templates'
+      local dirName = 'Templates'
 
       if string.find(filename, dirName) then
         return true
